0a07898c37fd9271601c61409c50dee6d6626ca0
BrodeArts <antoniodiaz1985@gmail.com>
2024-02-15 11:14:03 +0100

    Fin de la clase routers

--- Backend/FastApi/main.py
+++ Backend/FastApi/main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
 
+from routers import users, products
+
 app = FastAPI()
+
+app.include_router(users.router)
+app.include_router(products.router)
+
 
 # URL local: http://127.0.0.1:8000/
 
--- Backend/FastApi/routers/products.py
+++ Backend/FastApi/routers/products.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter
 
 router = APIRouter(prefix="/products", 
-                   tags="Products", 
-                   responses={404: "No encontrado"})
+                   tags=["Products"], 
+                   responses={404: {"message": "No encontrado"}})
 
 """
 Con prefix, le indicamos que la url "raiz" va a ser todo el rato /products, es decir, que en los get/post, etc solo debemos poner
@@ -14,12 +14,12 @@ para los productos y así queda mejor. Lo ideal será ponerle otro tag a "users"
 
 """
 
-product_list = ["Producto 1", "Producto 2", "Producto 3", "Producto 4", "Producto "]
+product_list = ["Producto 1", "Producto 2", "Producto 3", "Producto 4", "Producto 5"]
 
 @router.get("/") #Al haber usado el prefix en router, esta url sería: http://127.0.0.1:8000/products/
 async def products():
     return product_list
 
 @router.get("/{id}") #Al haber usado el prefix en router, esta url sería: http://127.0.0.1:8000/products/id_que_sea
-async def products():
-    return product_list
+async def products(id: int):
+    return product_list[id]

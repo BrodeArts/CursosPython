197cea365c60fe38ac35efb7db9d039d608b6b38
BrodeArts <antoniodiaz1985@gmail.com>
2024-02-23 10:29:54 +0100

    prueba nueva

--- Backend/FastApi/routers/jwt_auth_users.py
+++ Backend/FastApi/routers/jwt_auth_users.py
@@ -80,7 +80,7 @@ async def current_user(user: User = Depends(auth_user)):
     return user
 
 @router.post("/login")
-async def logi(form: OAuth2PasswordRequestForm = Depends()):
+async def login(form: OAuth2PasswordRequestForm = Depends()):
     user_db = users_db.get(form.username)
     if not user_db:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="El usuario no es correcto")

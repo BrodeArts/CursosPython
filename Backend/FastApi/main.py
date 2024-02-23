143b3be2031e575b7a125fd8776d5d137233f769
BrodeArts <antoniodiaz1985@gmail.com>
2024-02-23 10:25:53 +0100

    Prueba

--- Backend/FastApi/main.py
+++ Backend/FastApi/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
-
-from routers import users, products
+from routers import users, products, basic_auth_users, jwt_auth_users
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
 app.include_router(users.router)
 app.include_router(products.router)
+#app.include_router(basic_auth_users.router)
+app.include_router(jwt_auth_users.router)
 
 
 # URL local: http://127.0.0.1:8000/
@@ -31,4 +33,12 @@ async def url():
 #Thunder client
 
 ##Que es un CRUD? -> Create(post), Read(get), Update(put), Delete(delete)
+
+
+
+## RECURSOS ESTÁTICOS ##
+
+#De esta forma exponemos el directorio "static" y podremos acceder a los recursos que hay dentro con la ruta correcta:
+#http://127.0.0.1:8000/static/images/ternera.jpg
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
---
+++ Backend/FastApi/routers/jwt_auth_users.py
@@ -0,0 +1,106 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from pydantic import BaseModel
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import jwt, JWTError
+from passlib.context import CryptContext
+from datetime import datetime, timedelta
+
+router = APIRouter()
+
+ALGORITHM = "HS256"
+ACCESS_TOKEN_DURATION = 1
+
+#En vez de inventarme el secret, lo he generado así en la terminal >>openssl rand -hex 32
+SECRET = "eb08a0e6d2211023494a6e5267d38f022a44b6edfd869f5dfc0a6689cf2fd022"
+
+oauth2 = OAuth2PasswordBearer(tokenUrl="login")
+
+crypt = CryptContext(schemes=["bcrypt"])
+
+
+class User(BaseModel):
+    username: str
+    full_name: str
+    email: str
+    disabled: bool
+
+class UserDB(User):
+    password: str
+
+
+
+users_db = {
+    "brodearts": {
+        "username": "brodearts",
+        "full_name": "Antonio Díaz",
+        "email": "antonio@diazfernandez.es",
+        "disabled": False,
+        "password": "$2a$12$h4EPglXCd.KDhbU3LIOBSOEPcyPNBpgfFzqlNk.OBJ.NZMMToZxeu"
+    },
+    "brodearts2": {
+        "username": "brodearts2",
+        "full_name": "Antonio Díaz2",
+        "email": "antonio@diazfernandez.es2",
+        "disabled": True,
+        "password": "$2a$12$lGzIoyUj6BIwkJmH7grsw.JpU87G3Zg5oP99Gszmtf4TGSZTkwhOm"
+    }
+}
+
+def search_user_db(username: str):
+    if username in users_db:
+        return UserDB(**users_db[username])
+    
+def search_user(username: str):
+    if username in users_db:
+        return User(**users_db[username])
+    
+async def auth_user(token: str = Depends(oauth2)):
+
+    exception = HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, 
+            detail="Credenciales de autenticacion inválidas", 
+            headers={"WWW-Authenticate": "Bearer"})
+
+    try:
+        username = jwt.decode(token, SECRET, algorithms=[ALGORITHM]).get("sub")
+        if username is None:
+            raise exception
+
+
+    except JWTError:
+        raise exception
+        
+    return search_user(username)
+    
+async def current_user(user: User = Depends(auth_user)):
+    if user.disabled:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, 
+            detail="Usuario inactivo")
+    
+    return user
+
+@router.post("/login")
+async def logi(form: OAuth2PasswordRequestForm = Depends()):
+    user_db = users_db.get(form.username)
+    if not user_db:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="El usuario no es correcto")
+    
+    user = search_user_db(form.username)
+    if not crypt.verify(form.password, user.password):
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="La contraseña no es correcta")
+    
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_DURATION)
+
+    access_token = {"sub": user.username, "exp": expire, }
+
+    return {"access_token": jwt.encode(access_token, SECRET, algorithm=ALGORITHM), "token_type": "bearer"}
+
+
+@router.get("/users/me")
+async def me(user: User = Depends(current_user)):
+    return user
+
+
+
+
+
